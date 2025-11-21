88b0a970fd796e736ece8f5508720e54603f24b9
Obinna Emmanuel Duru <3878oed@gmail.com>
2025-11-21 10:37:55 +0100

    Create main.ts

---
+++ apps/api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true });
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`API running on http://localhost:${port}`);
+}
+bootstrap();
